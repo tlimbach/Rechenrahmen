8066082ed552927d2e80a9428b9b626c661e3cc7
tlimbach <49364223+tlimbach@users.noreply.github.com>
2024-11-06 22:56:21 +0100

    x

--- src/start.js
+++ src/start.js
@@ -27,51 +27,36 @@ window.onload = initialize;
 window.onresize = initialize;
 
 class Start {
+    constructor() {
+        const windowWidth = window.innerWidth - 20;
+        const windowHeight = window.innerHeight - 20;
 
-	constructor() {
+        const circleSize = windowHeight / 16;
+        const hGap = circleSize / 20;
+        const vGap = circleSize / 2;
 
-		const windowWidth = window.innerWidth-20;
-		const windowHeight = window.innerHeight-20;
-		
-		const circleSize = windowHeight / 16;
-		const hGap = circleSize/20;
-		const vGap = circleSize/2;
-		
-		const usedWidth = 11*(circleSize+hGap)*2;
-		const usedHeight = 10*(circleSize+vGap);
-		
-		const rowXPos = (windowWidth/2)-(usedWidth/2);
-		const rowYPos = (windowHeight/2)-(usedHeight/2);
-		
-		const colors = {
-			inner : '#eeeeee',
-			outer : 'lightgray',
-			circleColor1  :"#BE5A5D",
-			circleColor2  : "#4E4C8A",
-			colorStange : "#87ab62"
-		};
-//		const colors = {
-//			inner : '#fd0006',
-//			outer : 'purple',
-//			circleColor1  :"#d8f800",
-//			circleColor2  : "#269926",
-//			colorStange : "lightgray"
-//		};
+        const usedWidth = 11 * (circleSize + hGap) * 2;
+        const usedHeight = 10 * (circleSize + vGap);
 
-		this.paper = SVG().addTo('#canvasdiv').size(windowWidth, windowHeight);
-		
-		
-		const innerRect = this.paper.rect(usedWidth, usedHeight).move(rowXPos, rowYPos).radius(circleSize/2).fill(colors["inner"]);
-//		this.paper.rect(usedWidth, usedHeight).move(rowXPos, rowYPos).radius(10).fill(colors["inner"]);
-		
-		$('html').css('background-color', colors["outer"]);
-		
-		for (let row=0; row<10; row++) {
-			
-			this.paper.rect(usedWidth, 1).move(rowXPos, -1+circleSize/2+rowYPos +(vGap/2)+ (row*(circleSize+vGap))).fill(colors["colorStange"]);
-			
-			new CircleRow(this.paper, rowXPos+(hGap), rowYPos +(vGap/2)+ (row*(circleSize+vGap)), circleSize, hGap, vGap, usedWidth, usedHeight, colors);
-		}
+        const rowXPos = (windowWidth / 2) - (usedWidth / 2);
+        const rowYPos = (windowHeight / 2) - (usedHeight / 2);
 
-	}
+        const colors = {
+            inner: '#eeeeee',
+            outer: 'lightgray',
+            circleColor1: "#BE5A5D",
+            circleColor2: "#4E4C8A",
+            colorStange: "#87ab62"
+        };
+
+        this.paper = SVG().addTo('#canvasdiv').size(windowWidth, windowHeight);
+        const innerRect = this.paper.rect(usedWidth, usedHeight).move(rowXPos, rowYPos).radius(circleSize / 2).fill(colors["inner"]);
+
+        $('html').css('background-color', colors["outer"]);
+
+        for (let row = 0; row < 10; row++) {
+            this.paper.rect(usedWidth, 1).move(rowXPos, -1 + circleSize / 2 + rowYPos + (vGap / 2) + (row * (circleSize + vGap))).fill(colors["colorStange"]);
+            new CircleRow(this.paper, rowXPos + (hGap), rowYPos + (vGap / 2) + (row * (circleSize + vGap)), circleSize, hGap, vGap, usedWidth, usedHeight, colors);
+        }
+    }
 }
